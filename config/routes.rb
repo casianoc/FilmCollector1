74b48d5db1620757abb563e84c53cf84f1d6ca63
Cameron Casiano <casianoc@chc.edu>
2020-09-29 10:48:52 -0400

    Added delete, edit and show functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
   get "movies/new" => "movies#new"
   post "movies" => "movies#create"
   get "actors/:id" => "actors#show" 
+  get "movies/:id" => "movies#show"
+  get "actors/:id/edit" => "actors#edit"
+  patch "actors/:id" => "actors#update"
+  delete "actors/:id" => "actors#destroy"
+  get "movies/:id/edit" => "movies#edit"
+  patch "movies/:id" => "movies#update"
+  delete "movies/:id" => "movies#destroy"
 end
